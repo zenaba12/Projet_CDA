432d9e5e20a2ab0041f02c6781819dc1926c2b92
zenaba12 <alizenap433@gmail.com>
2025-02-16 07:38:21 +0100

    ajout de l'admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin')]
+#[IsGranted('ROLE_ADMIN')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'admin_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+}
