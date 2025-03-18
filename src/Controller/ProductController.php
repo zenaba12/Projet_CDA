e0774c1fbe8873ebbc1a25b0835df153a6b8166f
Zenaba <alizenap433@gmail.com>
2025-03-18 12:09:53 +0100

    Deploiement

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -116,7 +116,6 @@ class ProductController extends AbstractController
         ]);
     }
 
-    //  Modifier un produit
     #[Route('/edit/{id}', name: 'product_edit', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_ADMIN')]
     public function edit(int $id, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
@@ -126,6 +125,8 @@ class ProductController extends AbstractController
             throw $this->createNotFoundException('Produit non trouvé.');
         }
 
+        $existingImage = $product->getImage(); // Sauvegarde de l'image actuelle
+
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
@@ -134,20 +135,21 @@ class ProductController extends AbstractController
             $imageFile = $form->get('image')->getData();
 
             if ($imageFile) {
+                // Génération d'un nom sécurisé
                 $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
                 $safeFilename = $slugger->slug($originalFilename);
                 $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
 
                 try {
                     $imageFile->move($this->getParameter('images_directory'), $newFilename);
-                    $product->setImage($newFilename); // Mise à jour de l’image
+                    $product->setImage($newFilename); // Mise à jour de l’image avec la nouvelle
                 } catch (FileException $e) {
                     $this->addFlash('error', "Erreur lors de l'upload de l'image : " . $e->getMessage());
                     return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
                 }
             } else {
-                // IMPORTANT : Conserver l'ancienne image si aucun fichier n'est uploadé
-                $product->setImage($product->getImage());
+                // Si aucune nouvelle image n'est ajoutée, on conserve l'ancienne
+                $product->setImage($existingImage);
             }
 
             $em->flush();
@@ -155,67 +157,9 @@ class ProductController extends AbstractController
             return $this->redirectToRoute('product_index');
         }
 
-
         return $this->render('product/edit.html.twig', [
             'form' => $form->createView(),
             'product' => $product
         ]);
-    }
-
-    //  Supprimer un produit
-    #[Route('/delete/{id}', name: 'product_delete', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_ADMIN')]
-    public function delete(int $id, EntityManagerInterface $em, Request $request): Response
-    {
-        $product = $em->getRepository(Product::class)->find($id);
-        if (!$product) {
-            throw $this->createNotFoundException('Produit non trouvé.');
-        }
-
-        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token'))) {
-            $em->remove($product);
-            $em->flush();
-            $this->addFlash('success', 'Produit supprimé avec succès.');
-        }
-
-        return $this->redirectToRoute('product_index');
-    }
-    #[Route('/add-to-cart/{id}', name: 'product_add_to_cart')]
-    public function addToCart(int $id, EntityManagerInterface $em, Security $security): Response
-    {
-        $user = $security->getUser();
-        if (!$user) return $this->redirectToRoute('app_login');
-
-        $product = $em->getRepository(Product::class)->find($id);
-        if (!$product) {
-            throw $this->createNotFoundException("Produit non trouvé.");
-        }
-
-        // Vérifier si l'utilisateur a déjà un panier
-        $cart = $em->getRepository(Cart::class)->findOneBy(['user' => $user]);
-
-        if (!$cart) {
-            $cart = new Cart();
-            $cart->setUser($user);
-            $em->persist($cart);
-        }
-
-        // Vérifier si le produit est déjà dans le panier
-        $cartItem = $em->getRepository(CartItem::class)->findOneBy(['cart' => $cart, 'product' => $product]);
-
-        if ($cartItem) {
-            $cartItem->setQuantity($cartItem->getQuantity() + 1);
-        } else {
-            $cartItem = new CartItem();
-            $cartItem->setCart($cart);
-            $cartItem->setProduct($product);
-            $cartItem->setQuantity(1);
-            $em->persist($cartItem);
-        }
-
-        $em->flush();
-
-        $this->addFlash('success', 'Produit ajouté au panier.');
-        return $this->redirectToRoute('cart_show');
     }
 }
